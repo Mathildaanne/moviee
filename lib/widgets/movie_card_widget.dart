e961867d3bf1c54eba4fde9532e4061b52921ca1
Mathildaanne <mathildaanneke10@gmail.com>
2024-11-15 07:29:18 +0700

    third commit

--- lib/widgets/movie_card_widget.dart
+++ lib/widgets/movie_card_widget.dart
@@ -11,44 +11,45 @@ class MovieCardWidget extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return FutureBuilder(
-        future: future,
-        builder: (context, snapshot) {
-          if (snapshot.hasData) {
-            var data = snapshot.data?.results;
-            return Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Text(
-                  headLineText,
-                  style: const TextStyle(fontWeight: FontWeight.bold),
-                ),
-                const SizedBox(
-                  height: 20,
-                ),
-                Expanded(
-                  child: ListView.builder(
-                      itemCount: data?.length ?? 0,
-                      scrollDirection: Axis.horizontal,
-                      shrinkWrap: true,
-                      itemBuilder: (context, index) {
-                        if (data == null || index >= data.length) {
-                          return const SizedBox();
-                        }
-                        return Container(
-                          padding: EdgeInsets.all(5),
-                          decoration: BoxDecoration(
-                            borderRadius: BorderRadius.circular(20),
-                          ),
-                          child: Image.network(
-                              "${imageUrl}${data[index].posterPath}"),
-                        );
-                      }),
-                )
-              ],
-            );
-          } else {
-            return const SizedBox.shrink();
-          }
-        });
+      future: future,
+      builder: (context, snapshot) {
+        if (snapshot.hasData) {
+          var data = snapshot.data?.results;
+          return Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(
+                headLineText,
+                style: const TextStyle(fontWeight: FontWeight.bold),
+              ),
+              const SizedBox(
+                height: 20,
+              ),
+              Expanded(
+                child: ListView.builder(
+                    itemCount: data?.length ?? 0,
+                    scrollDirection: Axis.horizontal,
+                    shrinkWrap: true,
+                    itemBuilder: (context, index) {
+                      if (data == null || index >= data.length) {
+                        return const SizedBox();
+                      }
+                      return Container(
+                        padding: EdgeInsets.all(5),
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(20),
+                        ),
+                        child: Image.network(
+                            "${imageUrl}${data[index].posterPath}"),
+                      );
+                    }),
+              )
+            ],
+          );
+        } else {
+          return const SizedBox.shrink();
+        }
+      },
+    );
   }
 }
